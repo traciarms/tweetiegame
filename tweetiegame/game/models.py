9e4ffa0381e162ba0d4fd2e4a5506b9dbebe9ec6
Eric Turner <EricTurnerNV@gmail.com>
2015-08-22 10:57:48 -0700

    added fields to models

--- tweetiegame/game/models.py
+++ tweetiegame/game/models.py
@@ -10,3 +10,4 @@ class Game(models.Model):
     player1score = models.IntegerField(default=0)
     player2score = models.IntegerField(default=0)
     completed = models.BooleanField(default=False)
+    current_player_id = models.ForeignKey(User, related_name='current_player')
