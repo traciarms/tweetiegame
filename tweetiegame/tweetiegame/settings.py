fbedb22c24c95c0d03a30171fb14e6f44e675bc1
Traci Armstrong <traciarms@gmail.com>
2015-08-21 18:26:51 -0700

    initial commit

--- tweetiegame/tweetiegame/settings.py
+++ tweetiegame/tweetiegame/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'game'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'tweetiegame.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
