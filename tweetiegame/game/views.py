f4c55956dd627960283ebb65856c8896d5093971
Eric Turner <EricTurnerNV@gmail.com>
2015-08-21 20:22:18 -0700

    fixed some little things.

--- tweetiegame/game/views.py
+++ tweetiegame/game/views.py
@@ -1,10 +1,13 @@
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 import json
 from django.views.generic import TemplateView
-from tweetiegame.game.client import Client
+import requests
+from game.client import Client
+
 
 class SearchTwitterView(TemplateView):
     template_name = "index.html"
@@ -21,14 +24,11 @@ class SearchTwitterView(TemplateView):
 
         # Pretty print of tweet payload
         # tweet = client.request('https://api.twitter.com/1.1/statuses/show.json?id=316683059296624640')
-        context['tweet'] = client.request('https://api.twitter.com/1.1/search/tweets.json')
+        context['tweet'] = requests.get('https://api.twitter.com/1.1/search/tweets.json')
         # print(json.dumps(tweet, sort_keys=True, indent=4, separators=(',', ':')))
 
         # Show rate limit status for this application
         # status = client.rate_limit_status()
         # print(status['resources']['search'])
         return context
-
-    def index(request):
-        return HttpResponse('This is the index')
 
--- tweetiegame/tweetiegame/urls.py
+++ tweetiegame/tweetiegame/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-import game
+from game.views import SearchTwitterView
 
 urlpatterns = [
+    url(r'^$', SearchTwitterView.as_view(), name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/', auth_views.login, {'extra_context': {'next': '/'}}, name='login'),
-    url(r'^$', 'game.views.index', name='index'),
 ]
